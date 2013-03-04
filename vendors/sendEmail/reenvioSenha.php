714390482baf89fb32f8890079c023bfc8141fa6
dimas <dimas@dizain.com.br>
2013-03-04 17:52:11 -0300

    Pagina reenvio de senha.

---
+++ vendors/sendEmail/reenvioSenha.php
@@ -0,0 +1,137 @@
+
+<table align="center" width="550" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+	<td>
+		<table width="100%" border="0" cellspacing="0" cellpadding="0">
+		  <tr>
+			<td valign="top"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r1_c1_s1_s1.jpg" alt="KNAUF CLANEO AWARD 2013" title="KNAUF CLANEO AWARD 2013KNAUF CLANEO AWARD 2013" width="204" height="151" /></td>
+			<td valign="top"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r1_c3_s1_s1.jpg" alt="escreva o seu nome nas paredes da fama " title="escreva o seu nome nas paredes da fama " width="346" height="151" /></td>
+		  </tr>
+		</table>
+	</td>
+	</tr>
+	<tr>
+		<td height="27" bgcolor="#DAEBF2">&nbsp;</td>
+	</tr>
+	<tr>
+		<td height="29" bgcolor="#DAEBF2">
+			<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			  <tr>
+				<td width="200">&nbsp;</td>
+				<td valign="top" height="29" style="font-family:Arial, Helvetica, sans-serif;font-size:18px;font-weight:bold;">Reenvio de senha</td>
+			  </tr>
+			</table>
+		</td>
+	</tr>   
+	<tr>
+		<td bgcolor="#DAEBF2">
+			<table width="100%" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+					<td width="58">&nbsp;</td>
+					<td width="461">
+						<table width="100%" border="0" cellspacing="0" cellpadding="0">
+				
+							<tr>
+								<td>
+									<table width="100%" border="0" cellspacing="0" cellpadding="0">
+										<tr>
+											<td>
+												<table width="100%" border="0" cellspacing="0" cellpadding="0">
+													<tr>													
+														<td>
+															<table width="100%" border="0" cellspacing="0" cellpadding="0">
+																<tr>
+																	<td width="12">&nbsp;</td>
+																	<td style="font-family:Arial, Helvetica, sans-serif;font-size:14px;font-weight:bold;font-weight:bold;">Clique no link abaixo para resgatar sua senha:</td>
+																</tr>
+															</table>
+														</td>
+													</tr>
+													<tr>													
+														<td>
+															<table width="100%" border="0" cellspacing="0" cellpadding="0">
+																<tr>
+																	<td width="126">&nbsp;</td>
+																	<td style="font-family:Arial, Helvetica, sans-serif;font-size:14px;font-weight:bold;font-weight:bold;">&nbsp;</td>
+																</tr>
+															</table>
+														</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+										<tr>
+											<td height="35" valign="bottom">
+												<table width="100%" border="0" cellspacing="0" cellpadding="0">
+													<tr>
+														<td width="81">&nbsp;</td>
+														<td style="font-family:Arial, Helvetica, sans-serif;font-size:16px;color:#6F9FB5;"><a style="font-family:Arial, Helvetica, sans-serif;font-size:16px;color:#6F9FB5;font-weight:bold" href="'.ROOT.'/auth/'.md5($user['id']).$user['hash'].'" target="_blank">####</a></td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+										<tr>
+											<td height="20">
+												<table width="100%" border="0" cellspacing="0" cellpadding="0">
+													<tr>
+														<td width="205">&nbsp;</td>
+														<td valign="top" style="font-family:Arial, Helvetica, sans-serif;font-size:20px;font-weight:bold;color:#ffffff;">&nbsp;</td>													</tr>	
+												</table>
+											</td>
+										</tr>
+										<tr>
+											<td height="20">
+												<table width="100%" border="0" cellspacing="0" cellpadding="0">
+												</table>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</td>
+				<td width="31">&nbsp;</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+  <tr>
+    <td bgcolor="#DAEBF2">&nbsp;</td>
+  </tr>
+  
+  <tr>
+    <td bgcolor="#DAEBF2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>
+		<table width="100%" border="0" cellspacing="0" cellpadding="0">
+		  <tr>
+			<td width="165" height="122" valign="top"><a href="http://www.knauf.com.br/" target="_blank"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r3_c1_s1_s1.jpg" alt="KNAUF DRYWALL" title="KNAUF DRYWALL" width="165" height="122" /></a></td>
+			<td width="230" height="122" valign="top"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r3_c2_s1_s1.jpg" alt="SAK | Servi&ccedil;o de Atendimento Knauf - 0800 704 9922 - E-mail: sak@knauf.com.br" title="SAK | Servi&ccedil;o de Atendimento Knauf - 0800 704 9922 - E-mail: sak@knauf.com.br" width="230" height="122" /></td>
+			<td>
+				<table width="100%" border="0" cellspacing="0" cellpadding="0">				  <tr>
+					<td colspan="5" valign="top"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r3_c4_s1_s1.jpg" width="155" height="38" /></td>
+				  </tr>
+				  <tr>
+					<td colspan="5" valign="top"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r4_c4_s1_s1.jpg" alt="Acompanhe:" width="155" height="23" /></td>
+				  </tr>
+				  <tr>
+					<td width="22">&nbsp;</td>
+					<td width="34" height="35" valign="top"><a href="https://www.facebook.com/KnaufDoBrasil" target="_blank"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r5_c5_s1_s1.jpg" alt="facebook" title="facebook" width="34" height="35" /></a></td>
+					<td width="37" height="35" valign="top"><a href="https://twitter.com/intent/user?screen_name=knaufdobrasil" target="_blank"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r5_c6_s1_s1.jpg" alt="twitter" title="twitter" width="37" height="35" /></a></td>
+					<td width="34" height="35" valign="top"><a href="http://www.youtube.com/knaufbrasil" target="_blank"><img style="display:block" border="0"  src="'.ROOT.'/assets/images/news/img_r5_c7_s1_s1.jpg" alt="youtube" title="youtube" width="34" height="35" /></a></td>
+					<td width="35">&nbsp;</td>
+				  </tr>
+				  <tr>
+					<td height="26" colspan="5">&nbsp;</td>
+				  </tr>
+				</table>
+			</td>
+		  </tr>
+		</table>
+	</td>
+  </tr>
+</table>
